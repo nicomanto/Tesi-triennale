4a6bc6467f75ebdea222116703c86453da1c75d7
Niccolò Mantovani <72399987+nicomanto@users.noreply.github.com>
2021-09-09 09:19:09 +0200

    End tesi (#24)
    * text compilazione in folder
    
    * change build
    
    * add permision
    
    * test with ./
    
    * test build
    
    * test build 2
    
    * test build 3
    
    * test build 4
    
    * test build 5
    
    * test build 6
    
    * change compiler folder to root and start glossario and flink section
    
    * add some subsection and section about technology
    
    * add minted for code
    
    * test --shell-escape on biber
    
    * fix -shell
    
    * test
    
    * test -shell-escape before name
    
    * Add keyBy operator
    
    * Prima bozza (#16)
    
    * close
    
    * add myParagraph command
    
    * start datastream API and add processFunction
    
    * add datastream and datastream api
    
    * #3 - rimosse sottosezioni dall'indice, add table API
    
    * #4 - start keyed e broadcast processFunction
    
    * Add keyed and broadcast process function, checkpoint and savepoint and start serialization
    
    * close #4, close #5 - end Flink part
    
    * typo and some fix
    
    * close #3 - solo 3 livelli di profondità anche sull'intero documento
    
    * creazione adattamento operatori preesistenti
    
    * fix bboutput
    
    * fix position of table
    
    * #6 - change title
    
    * fix thesis index
    
    * close #7, close #8
    
    * End index and change name
    
    * Add Scala section
    
    * Add introduction
    
    * start refactor windowing
    
    * fix image/table and add pr1 on windowing operator
    
    * add pr2 and onTimer windowing
    
    * #9 - end operator windowing
    
    * #9 - start refactor alertCoProcess
    
    * #9 - end AlertCoProcess and fix selfAggregation on windowing
    
    * #12 - aggiunto testing with flink, add scalaTest
    
    * Add testing on operator
    
    * check introduzione
    
    * check english and tech terms into Strumenti e tecnologie
    
    * typo progettazione e codifica
    
    * #11 - fix description
    
    * close #11 - end description azienda
    
    * start adr and add API topic
    
    * #13 - fix windowind state
    
    * close #13 - fix state AlertCoProcess
    
    * add ADR on PR! windowing
    
    * add adr on pr2 windowing
    
    * end adr on windowing
    
    * add adr on AlertCoProcess
    
    * Add go and amazon kinesis
    
    * add sitografia, progettazione controlli API, fix table caption
    
    * fix requisiti desiderabili
    
    * add adr api
    
    * Add test api
    
    * start conclusioni
    
    * add use case
    
    * end conclusioni and sommario
    
    * change order language documentClass
    
    * remove example of template
    
    * test entry glossario
    
    * test
    
    * test
    
    * remove english
    
    * restore english
    
    * gls terms with textit, check cap 1 and 2
    
    * close #17 - Add classificazione casi d'uso
    
    * Check ADR
    
    * check codifica
    
    * check verifica, conclusioni e glossario
    
    * check intro and sommario
    
    * typo
    
    * check
    
    * create diagram class
    
    * check fino windowing (compresa) progettazione
    
    * check progettazione e codifica
    
    * final check
    
    * fix tutor check
    
    * close #20
    
    * close #19
    
    * Fix typo suggest by tutor interno
    
    * replace processamento con elaborazione
    
    * Fix Sommario
    
    * Fix example operators
    
    * start fix introduction
    
    * typo
    
    * fix detector and contesto in 4.1
    
    * fix conclusioni
    
    * close #18
    
    * Fix consigli Tutor e verifica ortografica

---
+++ clean.bat
@@ -0,0 +1,9 @@
+@echo off
+
+if [%BUILD_DIR%]==[] set BUILD_DIR=.\
+
+@echo --------------------------------------------------------------
+@echo -                         Pulizia                            -
+@echo --------------------------------------------------------------
+del %BUILD_DIR%*.sc %BUILD_DIR%*.acn %BUILD_DIR%*.acr %BUILD_DIR%*.alg %BUILD_DIR%*.aux %BUILD_DIR%*.bbl %BUILD_DIR%*.bcf %BUILD_DIR%*.blg %BUILD_DIR%*.glg %BUILD_DIR%*.glo %BUILD_DIR%*.gls
+del %BUILD_DIR%*.ist %BUILD_DIR%*.lof %BUILD_DIR%*.log %BUILD_DIR%*.lot %BUILD_DIR%*.run.xml %BUILD_DIR%*.synctex %BUILD_DIR%*.toc %BUILD_DIR%*-frn.tex 
